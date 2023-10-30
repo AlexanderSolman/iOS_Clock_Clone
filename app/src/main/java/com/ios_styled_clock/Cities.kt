3f19681db3a1623f9e0314e9229a2afeb6d2f1a2
AlexanderSolman <alexander.solman.andersson@gmail.com>
2023-10-30 14:00:59 +0100

    Updated cities class

--- app/src/main/java/com/ios_styled_clock/Cities.kt
+++ app/src/main/java/com/ios_styled_clock/Cities.kt
@@ -1,5 +1,6 @@
 package com.ios_styled_clock
 
+// Class to represent the entire list of selectable cities
 class Cities(private var city: String, private var country: String, private var timeZone: String) {
     override fun toString(): String {
         return "$city, $country"
